4ad5dbe09f946c83f9076e4aa9803c6ed582b2cb
Steven nguyen <nguyeste008@students.garlandisd.net>
2021-12-30 17:52:27 -0600

    Create edit.py

---
+++ edit.py
@@ -0,0 +1,22 @@
+
+
+file = open("in.txt")
+command, path, *words = file.read().split("\n")
+file.close()
+
+file = open(path)
+oldwords = file.read().split("\n")
+file.close()
+
+file = open(path, "w")
+if command == "add":
+    uniquewords = set(words + oldwords)
+    sortedwords = list(uniquewords)
+    sortedwords.sort()
+    filetext = "\n".join(set(words + oldwords))
+    file.write(filetext)
+elif command == "delete":
+    filteredwords = [word for word in oldwords if not word in words]
+    file.write("\n".join(filteredwords))
+
+file.close()

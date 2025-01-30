5546af98adfb3ee080d2fa4b599472f101f358ed
icecream17 <nguyeste008@students.garlandisd.net>
2025-01-30 11:22:55 -0600

    actually sort

--- edit.py
+++ edit.py
@@ -1,7 +1,5 @@
-
-
 file = open("in.txt")
-command, path, *words = file.read().split("\n")
+command, path, *words = file.read().split()
 file.close()
 
 file = open(path)
@@ -13,7 +11,7 @@ if command == "add":
     uniquewords = set(words + oldwords)
     sortedwords = list(uniquewords)
     sortedwords.sort()
-    filetext = "\n".join(set(words + oldwords))
+    filetext = "\n".join(sortedwords)
     file.write(filetext)
 elif command == "delete":
     filteredwords = [word for word in oldwords if not word in words]

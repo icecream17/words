39ce2413ed79ed822756bd0ce749e9270fedb252
Steven Nguyen <icecream17.github@gmail.com>
2025-06-14 12:50:47 -0500

    specific subject matter words are words

--- edit.py
+++ edit.py
@@ -1,19 +1,25 @@
+def update_file(command, path, words):
+    file = open(path)
+    oldwords = file.read().split("\n")
+    file.close()
+    
+    file = open(path, "w")
+    if command == "add":
+        uniquewords = set(words + oldwords)
+        sortedwords = list(uniquewords)
+        sortedwords.sort()
+        file.write("\n".join(sortedwords) + "\n")
+    elif command == "delete":
+        filteredwords = [word for word in oldwords if not word in words]
+        file.write("\n".join(filteredwords) + "\n")
+    
+    file.close()
+
+
+
 file = open("in.txt")
 command, path, *words = file.read().split()
 file.close()
 
-file = open(path)
-oldwords = file.read().split("\n")
-file.close()
-
-file = open(path, "w")
-if command == "add":
-    uniquewords = set(words + oldwords)
-    sortedwords = list(uniquewords)
-    sortedwords.sort()
-    file.write("\n".join(sortedwords) + "\n")
-elif command == "delete":
-    filteredwords = [word for word in oldwords if not word in words]
-    file.write("\n".join(filteredwords) + "\n")
-
-file.close()
+update_file(command, path, words)
+update_file(command, 'words.txt', words)

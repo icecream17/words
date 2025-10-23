787ec247150aee0ee02c7cc0272593b8acb4f1da
Steven Nguyen <icecream17.github@gmail.com>
2025-10-23 13:19:18 -0500

    fix file not found bug

--- edit.py
+++ edit.py
@@ -1,20 +1,27 @@
-def update_file(command, path, words):
-    file = open(path)
-    oldwords = file.read().split("\n")
-    file.close()
-    
-    file = open(path, "w")
-    if command == "add":
-        uniquewords = set(words + oldwords)
-        sortedwords = list(uniquewords)
-        sortedwords.sort()
-        file.write("\n".join(sortedwords) + "\n")
-    elif command == "delete":
-        filteredwords = [word for word in oldwords if not word in words]
-        file.write("\n".join(filteredwords) + "\n")
-    
-    file.close()
+def create_file(path):
+    try:
+        open(path, "x").close()
+    finally:
+        pass
 
+def read_lines(path):
+    file = open(path)
+    lines = file.read().split("\n")
+    file.close()
+    return lines
+
+def update_file(command, path, words):
+    create_file(path)
+    oldwords = set(read_lines(path))
+    words = set(words)
+    with open(path, "w") as file:
+        if command == "add":
+            uniquewords = words | oldwords
+            sortedwords = sorted(uniquewords)
+            file.write("\n".join(sortedwords) + "\n")
+        elif command == "delete":
+            filteredwords = oldwords - words
+            file.write("\n".join(filteredwords) + "\n")
 
 
 file = open("in.txt")
